c07b478fe17fd01f816c0e2ac6647ab5b4b18182
Hugo Vermaak <hugo@colossal.co.za>
2023-02-06 23:53:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Lekker UI
+
+**lekker**, _adjective and adverb_
+
+_/ˈlekə/, /ˈlɛkər/, /ˈlækə/, /ˈlʌkə/_
+
+---
+
+**Forms:** lecker, leke
+
+**Origin:** Afrikaans, Dutch
+
+colloquial
+
+---
+
+### A. adjective
+
+A term of general approbation: ‘nice’, ‘pleasant’, ‘good’, ‘lovely’.
+
+### B. adverb
+
+Well; delightfully
+
+---
+
+[Full Definition](https://www.dsae.co.za/entry/lekker/e04283)
+
+## Getting Started
+
+Coming soon ...
+
+### Dependencies
+
+- [Tailwind CSS](https://tailwindcss.com)
+- [VueUse](https://vueuse.org)
+- [Motion One](https://motion.dev)
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mx-auto max-w-lg flex flex-col justify-center align-middle h-full">
+    <div class="mb-8">
+      <h1 class="text-4xl font-bold">Lekker UI</h1>
+      <p>A customisable UI component library for VueJS 3 using Tailwind CSS</p>
+    </div>
+    <p class="text-emerald-500 text-2xl mb-4 border-b-2 pb-4">
+      <span class="font-bold">lekker</span>, <span class="italic">adjective and adverb</span>
+    </p>
+    <p class="mb-8">/ˈlekə/, /ˈlɛkər/, /ˈlækə/, /ˈlʌkə/</p>
+    <div class="mb-8">
+      <p><span class="font-bold text-stone-400">Forms:</span> lecker, leke</p>
+      <p><span class="font-bold text-stone-400">Origin:</span> Afrikaans, Dutch</p>
+      <p>colloquial</p>
+    </div>
+    <div class="mb-4">
+      <p class="text-emerald-500 italic">A. adjective</p>
+      <p>A term of general approbation: ‘nice’, ‘pleasant’, ‘good’, ‘lovely’.</p>
+    </div>
+    <div class="mb-4">
+      <p class="text-emerald-500 italic">B. adverb</p>
+      <p>Well; delightfully</p>
+    </div>
+    <p class="mt-2">
+      <a href="https://www.dsae.co.za/entry/lekker/e04283" target="_blank" class="text-blue-600 underline">
+        Full Definition
+      </a>
+    </p>
+    <p class="mt-8 text-center">
+      <a href="" class="bg-emerald-500 text-emerald-50 rounded-full px-6 py-4 font-medium">Getting Started</a>
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
